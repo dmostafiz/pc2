893bcb6051f605af44949709f92ab378c9262ab9
dmostafiz <dev.mostafiz@gmail.com>
2025-09-19 00:11:39 +0600

    okay

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        BASE_API: process.env.BASE_API,
+        STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
+    },
+};
 
 export default nextConfig;
